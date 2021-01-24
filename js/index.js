d4c866cf3fa73e475823056a9bdd241bfbf3dc21
muminae100 <smuminaetx100@gmail.com>
2021-01-24 16:20:35 +0300

    reduced the slide show size and add slide show info

--- js/index.js
+++ js/index.js
@@ -1,31 +1,28 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+var slideshows = document.querySelectorAll('[data-component="slideshow"]');
+  
+// Apply to all slideshows that you define with the markup wrote
+slideshows.forEach(initSlideShow);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function initSlideShow(slideshow) {
+
+  var slides = document.querySelectorAll(`#${slideshow.id} [role="list"] .slide`); // Get an array of slides
+
+  var index = 0, time = 10000;
+  slides[index].classList.add('active');  
+  
+  setInterval( () => {
+    slides[index].classList.remove('active');
+    
+    //Go over each slide incrementing the index
+    index++;
+    
+    // If you go over all slides, restart the index to show the first slide and start again
+    if (index === slides.length) index = 0; 
+    
+    slides[index].classList.add('active');
+
+  }, time);
 }
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-} 
 function openNav() {
   document.getElementById("mySidenav").style.width = "350px";
   document.body.style.backgroundColor = "rgba(0,0,0, .4)";
@@ -76,3 +73,6 @@ getArticles();
 
 //   })
 // }
+
+
+
