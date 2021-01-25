3d8dd48639faa89448b1627cb95ac70f442be82a
muminae100 <smuminaetx100@gmail.com>
2021-01-25 22:20:25 +0300

    fixed the slide show section

--- js/index.js
+++ js/index.js
@@ -25,13 +25,11 @@ function initSlideShow(slideshow) {
 }
 function openNav() {
   document.getElementById("mySidenav").style.width = "350px";
-  document.body.style.backgroundColor = "rgba(0,0,0, .4)";
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
-  document.body.style.backgroundColor = "white";
 } 
 
 function getAllArticles(){
